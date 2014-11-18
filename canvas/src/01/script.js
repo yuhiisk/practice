ac5424469045439ece51a3f023dbb60e438a9817
Youhei Isokawa <isokawa-yohei@kayac.com>
2014-11-18 10:20:34 +0900

    commit 01

--- canvas/src/01/script.js
+++ canvas/src/01/script.js
@@ -1,5 +1,11 @@
 ;(function(win, doc) {
 
-  'use strict';
+    'use strict';
+
+    win.onload = function() {
+        var canvas = doc.getElementById('canvas'),
+            ctx = canvas.getContext('2d');
+
+    };
 
 })(this, this.document);
