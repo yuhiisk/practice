a2c4274cedc474e73c87af614b84e2bab894fc05
yuhiisk <youheiisokawa@gmail.com>
2014-11-22 12:51:40 +0900

    modified paint/

--- canvas/src/paint/js/main.js
+++ canvas/src/paint/js/main.js
@@ -5,11 +5,12 @@
 
   var lineWidth = 1,
     lineHeight = 1,
-    drawFlag = false,
     oldX = 0,
     oldY = 0;
 
-  doc.addEventListener('DOMContentLoaded', function() {
+  doc.addEventListener('DOMContentLoaded', init, false);
+
+  function init() {
 
     var resetButton = doc.getElementById('reset'),
       saveButton = doc.getElementById('save'),
@@ -18,18 +19,16 @@
 
     ctx.strokeStyle = 'rgba(255, 0, 0, 1)';
 
-    canvas.addEventListener('mousemove', draw, true);
     canvas.addEventListener('mousedown', function(e) {
-      drawFlag = true;
       oldX = e.clientX;
       oldY = e.clientY;
+      canvas.addEventListener('mousemove', draw, true);
     }, false);
     canvas.addEventListener('mouseup', function(e) {
-      drawFlag = false;
+      canvas.removeEventListener('mousemove', draw, true);
     }, false);
 
     function draw(e) {
-      if (!drawFlag) { return; }
 
       var x = e.clientX,
         y = e.clientY;
@@ -39,25 +38,24 @@
         ctx.moveTo(oldX, oldY);
         ctx.lineTo(x, y);
         ctx.stroke();
-        ctx.closePath();
         oldX = x;
         oldY = y;
     }
 
-    resetButton.addEventListener('click', reset, false);
+    resetButton.addEventListener('click', _reset, false);
 
-    function reset() {
+    function _reset() {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
     }
 
-    saveButton.addEventListener('click', save, false);
+    saveButton.addEventListener('click', _save, false);
 
-    function save() {
+    function _save() {
       var data = canvas.toDataURL('image/png');
       data.replace('image/png', 'image/octet-stream');
       win.open(data, 'save');
     }
+  }
 
-  }, false);
 
 })(this, this.document);
